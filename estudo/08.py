0d8ab2666ed257919ef1b0708d506b655ceb203b
unknown <peposs06@outlook.com>
2024-04-03 17:51:41 -0300

    organização

--- estudo/08.py
+++ estudo/08.py
@@ -9,12 +9,15 @@ Triângulo Equilátero: três lados iguais;
 Triângulo Isósceles: quaisquer dois lados iguais;
 Triângulo Escaleno: três lados diferentes.
 """
+base = float(input("Digite o valor da base: "))
 lado_1 = float(input("Digite o valor do lado 1: "))
 lado_2 = float(input("Digite o valor do lado 2: "))
-lado_3 = float(input("Digite o valor do lado 3: "))
-if lado_1 == lado_2 == lado_3:
-    print("É um triângulo equilátero.")
-elif lado_1 == lado_2 or lado_2 == lado_3 or lado_1 == lado_3:
-    print("É um triângulo isóceles.")
+if base <= lado_1 + lado_2:
+    if base == lado_1 == lado_2:
+        print("É um triângulo equilátero.")
+    elif base == lado_1 or lado_2 == lado_1 or base == lado_2:
+        print("É um triângulo isóceles.")
+    else:
+        print("É um triângulo escaleno.")
 else:
-    print("É um triângulo escaleno.")
+    print("Esse triangulo não pode ser formado")
